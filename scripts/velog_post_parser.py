ad4c24e10edcdd353bd194b07065bbe1d95a6881
mandoo <160207399+JuYoungJun@users.noreply.github.com>
2024-05-17 14:10:24 +0900

    Update velog_post_parser.py

--- scripts/velog_post_parser.py
+++ scripts/velog_post_parser.py
@@ -1,6 +1,5 @@
 import os
 import requests
-from bs4 import BeautifulSoup
 
 # GitHub Personal Access Token 가져오기
 GH_PAT = os.getenv('GH_PAT')
@@ -23,7 +22,7 @@ if response.status_code == 200:
 
     # 각 파일의 제목 가져오기
     for item in data:
-        if item["type"] == "file":
+        if item["type"] == "file" and item["name"].endswith(".md"):
             file_name = item["name"]
             post_title = file_name.replace(".md", "")
             post_link = f"- [{post_title}]({velog_posts_url}/{file_name})\n"  # 포스트 제목으로 링크 생성
@@ -37,14 +36,16 @@ if response.status_code == 200:
         end_index = content.find("###", start_index + 1) if start_index != -1 else -1
         
         if start_index != -1 and end_index != -1:
-            new_content = content[:end_index] + new_content + content[end_index:]
+            updated_content = content[:end_index] + new_content + content[end_index:]
         else:
-            new_content = content + "\n\n### Velog Posts\n\n" + new_content
+            updated_content = content + "\n\n### Velog Posts\n\n" + new_content
             
         readme_file.seek(0)
-        readme_file.write(new_content)
+        readme_file.write(updated_content)
         readme_file.truncate()
         
     print("README.md 파일이 성공적으로 수정되었습니다.")
 else:
     print("GitHub API로 파일 목록을 가져오는 동안 오류가 발생했습니다.")
+    print(f"상태 코드: {response.status_code}")
+    print(f"응답 내용: {response.text}")
